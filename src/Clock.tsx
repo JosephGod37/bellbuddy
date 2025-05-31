9e748610a8a790195385b3171abfff4ca12b922e
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2025-05-31 21:41:19 +0200

    clok kompoment

---
+++ src/Clock.tsx
@@ -0,0 +1,14 @@
+﻿import React, { useEffect, useState } from 'react';
+
+const Clock: React.FC = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(new Date()), 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return <h2>Czas: {time.toLocaleTimeString()}</h2>;
+};
+
+export default Clock;
