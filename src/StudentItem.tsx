ef1ef10a2fdb80bc8cff49ed57a3245496f7e84c
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2025-05-31 22:05:02 +0200

    student component

---
+++ src/StudentItem.tsx
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import { Student } from './App';
+
+interface Props {
+    student: Student;
+    onToggle: (id: number) => void;
+}
+
+const StudentItem: React.FC<Props> = ({ student, onToggle }) => {
+    return (
+        <li style={{ marginBottom: 10 }}>
+            {student.name} –
+            <button
+                onClick={() => onToggle(student.id)}
+                style={{
+                    marginLeft: 10,
+                    backgroundColor: student.present ? 'green' : 'red',
+                    color: 'white'
+                }}
+            >
+                {student.present ? 'Obecny' : 'Nieobecny'}
+            </button>
+        </li>
+    );
+};
+
+export default StudentItem;
---
+++ src/StudentList.tsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { Student } from './App';
+import StudentItem from './StudentItem';
+
+interface Props {
+    students: Student[];
+    onToggle: (id: number) => void;
+}
+
+const StudentList: React.FC<Props> = ({ students, onToggle }) => {
+    return (
+        <ul>
+            {students.map((student) => (
+                <StudentItem
+                    key={student.id}
+                    student={student}
+                    onToggle={onToggle}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default StudentList;
